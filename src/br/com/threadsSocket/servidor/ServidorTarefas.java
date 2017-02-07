d488d22514c6235ae0728ed39ec60ca5ac438d5e
Daniel Souza da Silva <dssilva@padtec.com.br>
2017-02-07 13:10:49 -0200

    first
    Change-Id: Ic05a86d98c59fa6c6152a6452c696c96c4447142

---
+++ src/br/com/threadsSocket/servidor/ServidorTarefas.java
@@ -0,0 +1,21 @@
+package br.com.threadsSocket.servidor;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServidorTarefas {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("---- Iniciando Servidor ----");
+        ServerSocket servidor = new ServerSocket(12345);
+        while (true) {
+            Socket socket = servidor.accept();    
+            System.out.println("Aceitando novo cliente na porta " + socket.getPort());
+            DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
+            new Thread(distribuirTarefas).start();
+        }
+    }
+}
+
+
